eaef3b4976ac4176b72c45ce9b71d32678116edc
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-08-17 17:02:54 +0300

    added change

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,13 @@
 /** @format */
 
-import React, { Suspense } from "react";
-import ContentSwitcher from "../../contentSwitcher/ContentSwitcher";
-import { headerRoutes } from "../../routes/headerRoutes";
+import React, { Suspense } from 'react';
+import Loader from 'react-loader-spinner';
+import ContentSwitcher from '../../contentSwitcher/ContentSwitcher';
+import { headerRoutes } from '../../routes/headerRoutes';
 
 const Main = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <ContentSwitcher routes={headerRoutes} />
     </Suspense>
   );
