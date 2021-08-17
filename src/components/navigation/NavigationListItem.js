db4fb2a51002a1280f0b55c862f10ecfc228f679
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-08-17 14:02:15 +0300

    added project body

---
+++ src/components/navigation/NavigationListItem.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const NavigationListItem = () => {
+  return <></>;
+};
+
+export default NavigationListItem;
---
+++ src/contentSwitcher/ContentSwitcher.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import { Switch } from "react-router-dom";
+
+const ContentSwitcher = () => {
+  return <Switch></Switch>;
+};
+
+export default ContentSwitcher;
