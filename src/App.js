7dab43dd2206cdfcf4913e000eb09e64c6705277
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-18 16:33:51 +0200

    redux for planning-page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Contacts from './pages/contacts/Contacts';
+import Planning from './pages/planning/Planning';
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Planning />
       <Contacts />
       <Footer />
     </div>
--- src/pages/planning/Planning.js
+++ src/pages/planning/Planning.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import styles from './Planning.module.css';
+import { getTasks } from './redux/selectors';
+import { addTask } from './redux/operation';
+
+const initialState = {
+  title: '',
+  reward: 0,
+};
 
 const Planning = () => {
-  return <h2>Planning</h2>;
+  const [task, setTask] = useState(initialState);
+  console.log('~ task', task);
+
+  const dispatch = useDispatch();
+
+  const onAddTask = useCallback(
+    ({ title, reward }) => {
+      console.log('~ reward', reward);
+      console.log('~ title', title);
+
+      dispatch(addTask(title, reward));
+    },
+    [dispatch],
+  );
+
+  const onHandleChange = event => {
+    const { name, value } = event.target;
+    setTask(prev => ({ ...prev, [name]: value }));
+  };
+
+  const onHandleSubmit = useCallback(
+    event => {
+      event.preventDefault();
+      onAddTask(task);
+      setTask({ title: '', reward: 0 });
+    },
+    [task, onAddTask],
+  );
+  return (
+    <form onSubmit={onHandleSubmit} autoComplete="on">
+      <input
+        type="text"
+        name="title"
+        value={task.title}
+        minLength="3"
+        required
+        onChange={onHandleChange}
+        label="title"
+      />
+      <input
+        type="number"
+        name="reward"
+        onChange={onHandleChange}
+        value={task.reward}
+        minLength="3"
+        required
+        label="reward"
+      />
+      <button type="submit" className="registerBtn">
+        отправить
+      </button>
+    </form>
+  );
+
+  // <PlanningPoints />
+  // <NewTaskModal/>
+  // <AddCustomTask/>
 };
 
 export default Planning;
