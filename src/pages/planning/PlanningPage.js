f05692b31171473d0ccec0bedc5030e1318f6644
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-19 01:42:16 +0200

    styles planning-points

--- src/pages/planning/PlanningPage.js
+++ src/pages/planning/PlanningPage.js
@@ -3,13 +3,17 @@ import { useDispatch } from 'react-redux';
 import styles from './Planning.module.css';
 import { getTasks } from './redux/selectors';
 import { addTask } from './redux/operation';
+import PlanningPoints from '../../components/planningPoints/PlanningPoints';
+import PlanningCards from '../../components/planningCards/PlanningCards';
 
 const initialState = {
   title: '',
   reward: 0,
+  imageUrl: '',
 };
 
 const PlanningPage = () => {
+  // create task for form
   const [task, setTask] = useState(initialState);
   console.log('~ task', task);
 
@@ -34,12 +38,50 @@ const PlanningPage = () => {
     event => {
       event.preventDefault();
       onAddTask(task);
-      setTask({ title: '', reward: 0 });
+      setTask({ ...initialState });
     },
     [task, onAddTask],
   );
+
+  // select week
+
+  // const selectDay = e => {
+  //   const week = e.target.value;
+  //   setWeek(date);
+  // };
+
+  // get tasks
+  // только активные или все таски?
+  // const tasks = useSelector(getActiveTask);
   return (
-    <form onSubmit={onHandleSubmit} autoComplete="on">
+    <>
+      <div>
+        <h2 className={styles.planningTitle}>План на неделю:</h2>
+        <PlanningPoints />
+        <div>
+          <select>
+            <option>17-24</option>
+          </select>
+        </div>
+      </div>
+      <PlanningCards />
+      {/*<NewTaskModal />
+      <AddCustomTask /> */}
+    </>
+  );
+};
+
+export default PlanningPage;
+
+/* <select value={week} onChange={selectweek}>
+  {weeks.list.map(lang => (
+    <option value={week} key={week}>
+      17-24
+    </option>
+  ))}
+</select>; */
+
+/* <form onSubmit={onHandleSubmit} autoComplete="on">
       <input
         type="text"
         name="title"
@@ -61,12 +103,4 @@ const PlanningPage = () => {
       <button type="submit" className="registerBtn">
         отправить
       </button>
-    </form>
-  );
-
-  // <PlanningPoints />
-  // <NewTaskModal/>
-  // <AddCustomTask/>
-};
-
-export default PlanningPage;
+    </form> */
