b147e72868542079447afa5842a7389d274ccc36
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-08-18 14:12:56 +0300

    added navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,19 @@
 /** @format */
 
-import React from "react";
-import Navigation from "../navigation/Navigation";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { mainRoutes } from '../../routes/mainRoutes';
+import Navigation from '../navigation/Navigation';
+import styles from './Header.module.css';
 
 const Header = () => {
-  return <Navigation />;
+  const location = useLocation();
+
+  return (
+    <header className={styles.header}>
+      <Navigation routes={mainRoutes} prevPathname={location.pathname} />
+    </header>
+  );
 };
 
 export default Header;
