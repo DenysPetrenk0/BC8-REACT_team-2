4b9606198800e2322b906b031ab96bcf3e42f523
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-19 14:21:13 +0200

    days fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 /** @format */
 
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import Loader from 'react-loader-spinner';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import ContentSwitcher from './contentSwitcher/ContentSwitcher';
 import { mainRoutes } from './routes/mainRoutes';
 import CardList from './components/cards/CardList/CardList';
+import { getUserInfo } from './redux/auth/authOperations';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
+
   return (
     <div className="container">
       <Header />
