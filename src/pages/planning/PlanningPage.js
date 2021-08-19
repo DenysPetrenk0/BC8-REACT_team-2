53af40722dfa3b79577181df6109726cf56aee3b
Alina Akompi <alina.akompi@gmail.com>
2021-08-19 03:45:59 +0300

    add changes

--- src/pages/planning/PlanningPage.js
+++ src/pages/planning/PlanningPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import NewTaskModal from '../../components/taskModal/newTaskModal/NewTaskModal';
 
 const PlanningPage = () => {
   return (
     <>
       <h2>Planning</h2>
-      <NewTaskModal />
     </>
   );
 };
