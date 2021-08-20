2c09bb3a5e81577c53420ea4ab89948eb1621eee
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-20 10:53:49 +0200

    planning-days-logic

--- src/redux/tasks/tasksReducer.js
+++ src/redux/tasks/tasksReducer.js
@@ -1,10 +1,22 @@
 import { createReducer } from '@reduxjs/toolkit';
+import {
+  getUserInfoSuccess,
+  loginSuccess,
+  registerSuccess,
+} from '../auth/authActions';
 // combineReducers,
 import { createTaskSuccess, patchActiveTaskSuccess } from './tasksAction';
 
 const tasksReducer = createReducer([], {
   [createTaskSuccess]: (state, action) => [action.payload, ...state],
-  // [patchActiveTaskSuccess]: (state, action) => [action.payload, ...state],
+  [registerSuccess]: (_, action) => action.payload.week.tasks,
+  [loginSuccess]: (_, action) => action.payload.week.tasks,
+  [getUserInfoSuccess]: (_, action) => action.payload.week.tasks,
+  [patchActiveTaskSuccess]: (state, { payload }) =>
+    state.map(item => {
+      if (item._id === payload.id) return payload;
+      return item;
+    }),
 });
 
 export default tasksReducer;
