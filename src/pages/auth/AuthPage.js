f0d5563e27bd902b6d2fc8363a907160331c7bc6
serhiibulynin <serhiibulynin007@gmail.com>
2021-08-23 20:40:27 +0300

    added footer styles

--- src/pages/auth/AuthPage.js
+++ src/pages/auth/AuthPage.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import AuthForm from '../../components/authForm/AuthForm';
 import styles from './AuthPage.module.css';
+import Footer from '../../components/footer/Footer';
+import useWindowDimensions from '../planning/hooks/wirthHook';
 
 const AuthPage = () => {
+  const { width } = useWindowDimensions();
   return (
     <div className={styles.container}>
       <AuthForm />
+      {width > 579 && <Footer />}
     </div>
   );
 };
