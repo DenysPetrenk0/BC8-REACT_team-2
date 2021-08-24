ec383dbc134290d74c3d435cdaa078038ba43970
Alina Akompi <alina.akompi@gmail.com>
2021-08-24 23:00:14 +0300

    add changes

--- src/components/languageSwitcher/LanguageSwitcher.js
+++ src/components/languageSwitcher/LanguageSwitcher.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import styles from './LanguageSwitcher.module.css';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
   return (
     <div>
-      <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
-      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      <button
+        className={styles.languageButton}
+        onClick={() => i18n.changeLanguage('ru')}
+      >
+        RU
+      </button>
+      <button
+        className={styles.languageButton}
+        onClick={() => i18n.changeLanguage('en')}
+      >
+        EN
+      </button>
     </div>
   );
 };
