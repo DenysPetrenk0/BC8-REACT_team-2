42ba46a4386a289225a19bcd2c6ba096b61e7230
Alina Akompi <alina.akompi@gmail.com>
2021-08-24 18:22:10 +0300

    add translations

--- src/components/planningPoints/PlanningPoints.jsx
+++ src/components/planningPoints/PlanningPoints.jsx
@@ -1,19 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import styles from './PlanningPoints.module.css';
 import useWindowDimensions from '../../pages/planning/hooks/widthHook';
 
 const PlanningPoints = ({ tasks }) => {
   const { width } = useWindowDimensions();
+  const { t } = useTranslation();
   let sum = 0;
   tasks.map(task => (sum = sum + task.reward));
   return (
     <div className={styles.pointsContainer}>
       <p className={styles.points}>
-        {width > 579 && 'Определены задачи на'}
+        {width > 579 && t('Defined tasks')}
         <span className={styles.sumPoints}>{sum}</span>
-        {sum === 1 && 'бал'}
-        {sum > 1 && sum < 5 && sum > 21 && 'балла'}
-        {sum >= 5 && 'баллов'}
-        {sum === 0 && 'баллов'}
+        {sum === 1 && t('point1')}
+        {sum > 1 && sum < 5 && sum > 21 && t('point')}
+        {sum >= 5 && t('points')}
+        {sum === 0 && t('points')}
       </p>
     </div>
   );
