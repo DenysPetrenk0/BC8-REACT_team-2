01ff6954f57974939e32bc1ef7cfba60f937b1db
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-26 01:21:58 +0200

    switcher-themes-awards

--- src/components/awards/awardsTitle/AwardsTitle.js
+++ src/components/awards/awardsTitle/AwardsTitle.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import sprite from './sprite.svg';
 import styles from './AwardsTitle.module.css';
+import { ThemeContext } from '../../../App';
+import cx from 'classnames';
 
 const AwardsTitle = () => {
+  const { theme } = useContext(ThemeContext);
   const { t } = useTranslation();
   return (
     <div className={styles.AwardsTitle__Wrapper}>
       <svg className={styles.AwardsTitle__icon}>
         <use href={sprite + '#icon-gift-box'} width="30"></use>
       </svg>
-      <h2 className={styles.AwardsTitle}>{t('My gifts')}</h2>
+      <h2 className={cx(styles.AwardsTitle, styles[theme.colors.text])}>
+        {t('My gifts')}
+      </h2>
     </div>
   );
 };
