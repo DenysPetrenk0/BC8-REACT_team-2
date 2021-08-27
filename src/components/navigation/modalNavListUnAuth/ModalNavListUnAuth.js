31f9d8fc38da5a41a4e865b26acc11bfe72f7667
Vladyslav Bochkovskyi <bochkovskyi.vladyslav@gmail.com>
2021-08-27 10:41:24 +0300

    add close after click on unAuthorized

--- src/components/navigation/modalNavListUnAuth/ModalNavListUnAuth.js
+++ src/components/navigation/modalNavListUnAuth/ModalNavListUnAuth.js
@@ -2,22 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './ModalNavListUnAuth.module.css';
 
-const ModalNavListUnAuth = () => {
+const ModalNavListUnAuth = ({ onClose }) => {
   return (
     <div className={styles.container}>
       <NavLink
         className={styles.nav__link_item}
         activeClassName={styles.nav__link_item_active}
-        to="/"
-        exact
-      >
-        Главная
-      </NavLink>
-      <NavLink
-        className={styles.nav__link_item}
-        activeClassName={styles.nav__link_item_active}
         to="/auth"
         exact
+        onClick={onClose}
       >
         Авторизация
       </NavLink>
@@ -26,6 +19,7 @@ const ModalNavListUnAuth = () => {
         activeClassName={styles.nav__link_item_active}
         to="/contacts-us"
         exact
+        onClick={onClose}
       >
         Контакты
       </NavLink>
